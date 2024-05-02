6c808efcc6bb236cccf1f8996085ce2d5ce56007
LukeLowson <19080@my.sanctamaria.school.nz>
2024-05-02 13:35:29 +1200

    Fixed navbar

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html", user=current_user)
