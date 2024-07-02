7a7b70f67ab140a3504dc861650a0904dbea2daa
LukeLowson <19080@my.sanctamaria.school.nz>
2024-07-02 12:04:06 +1200

    Added Notes Page

--- website/auth.py
+++ website/auth.py
@@ -27,7 +27,7 @@ def login():
         
     data = request.form
     print(data)
-    return render_template("login.html", boolean=True)
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -62,4 +62,4 @@ def sign_up():
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
         
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
