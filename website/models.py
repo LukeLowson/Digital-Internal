0f97100edafb06f764484bea69d711936afc6938
LukeLowson <19080@my.sanctamaria.school.nz>
2024-08-02 11:00:19 +1200

    post files

--- website/models.py
+++ website/models.py
@@ -9,9 +9,17 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     notes = db.relationship('Note')
+    music = db.relationship('Music', backref='user', passive_deletes=True)
     
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+class Music(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(100000), nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, Music
 from . import db
 
 
@@ -20,3 +20,18 @@ def home():
             db.session.commit()
             flash('Note added!', category='success')
     return render_template("home.html", user=current_user)
+
+@views.route("/upload_music", methods=['GET', 'POST'])
+def upload_music():
+    if request.method == 'POST':
+        file = request.form.get('file')
+        
+        if not file:
+            flash('Please select a music file.', category='error')
+        else:
+            music = Music(file=file, user_id=current_user.id)
+            db.session.add(music)
+            db.session.commit()
+            flash('Music posted!', category='success')
+        
+    return render_template("music.html", user=current_user)
